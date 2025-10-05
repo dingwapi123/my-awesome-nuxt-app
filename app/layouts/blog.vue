f8916ea459e183dbcaa95acdc561a7c81a0dc4a0
cxz <1054757289@qq.com>
2025-10-05 16:15:09 +0800

    fix: 修复导航菜单项传递方式错误
    移除多余的数组包裹并删除已弃用的collapsed属性

--- app/layouts/blog.vue
+++ app/layouts/blog.vue
@@ -39,8 +39,7 @@
         <div class="container mx-auto px-4 py-2">
           <UNavigationMenu
             orientation="vertical"
-            collapsed
-            :items="[navItems]"
+            :items="navItems"
           />
         </div>
       </div>
